d46e5a52a281bb0a94fc5c6e367c01055997d6f4
lilianli17 <jieli1017@gmail>
2022-12-17 23:43:51 -0500

    Update code

--- ngram_test.go
+++ ngram_test.go
@@ -20,7 +20,7 @@ func TestTokenization2(t *testing.T) {
 	sen := "  This is an example. "
     actualList := Tokenize(2, sen)
 	fmt.Printf("%v", actualList)
-    expectedlist := []string{"<s>", "This", "is", "an", "example", ".", "</s>"}
+    expectedlist := []string{"<s>", "<s>", "This", "is", "an", "example", ".", "</s>", "</s>"}
     if reflect.DeepEqual(actualList, expectedlist){
         t.Errorf("Expected String(%s) is not same as"+
          " actual string (%s)", expectedlist, actualList)
